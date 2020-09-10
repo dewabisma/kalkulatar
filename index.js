5be61c3b4ee2d58ee7cef45acd009e333b51c42e
Dewa Bisma <idewagedebisma@gmail.com>
2020-09-10 19:31:24 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// Rendering view
+
+const calculationHistory = document.querySelector('#calculation-history');
+const calculationResult = document.querySelector('#calculation-result');
+const noHistoryText = document.querySelector('#no-history');
+
+// Rendering number buttons
+
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const zero = document.querySelector('#zero');
+
+// Rendering number modifier buttons
+
+const plusMinus = document.querySelector('#plus-minus');
+const comma = document.querySelector('#comma');
+const clearEntry = document.querySelector('#clear-entry-btn');
+const clear = document.querySelector('#clear-btn');
+const backspace = document.querySelector('#backspace');
+
+// Rendering operator buttons
+
+const plus = document.querySelector('#plus');
+const minus = document.querySelector('#minus');
+const multiplication = document.querySelector('#multiplication');
+const division = document.querySelector('#division');
+const cubic = document.querySelector('#cubic');
+const square = document.querySelector('#square');
+const squareRoot = document.querySelector('#square-root');
+const percentage = document.querySelector('#percentage');
+const equal = document.querySelector('#equal');
+
+// Method
+
+const method
