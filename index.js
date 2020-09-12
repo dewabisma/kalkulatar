e997df8dac232359d6a059990a7fc072bfde5bf1
Dewa Bisma <idewagedebisma@gmail.com>
2020-09-12 14:28:10 +0800

    number button function

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Global Variable
+let tempNumber = '0';
+let resultValue = '0';
+
 // Rendering view
 
 const calculationHistory = document.querySelector('#calculation-history');
@@ -39,4 +43,53 @@ const equal = document.querySelector('#equal');
 
 // Method
 
-const method
+const setResult = (type = 'CE') => {
+  if (type === 'CE') {
+    calculationResult.innerHTML = tempNumber;
+  } else calculationResult.innerHTML = resultValue;
+};
+const resetValue = (source) => {
+  tempNumber = '0';
+  setResult(source.target.innerHTML);
+};
+const resetResult = (source) => {
+  resultValue = '0';
+  tempNumber = '0';
+  setResult(source.target.innerHTML);
+};
+const modifyValue = (tes) => {
+  if (tempNumber.length < 21) {
+    if (tempNumber === '0') {
+      tempNumber = String(tes.target.innerHTML);
+    } else tempNumber += String(tes.target.innerHTML);
+    setResult();
+  }
+};
+
+// Testing Number Button
+
+setResult();
+
+one.addEventListener('click', (event) => modifyValue(event));
+
+two.addEventListener('click', (event) => modifyValue(event));
+
+three.addEventListener('click', (event) => modifyValue(event));
+
+four.addEventListener('click', (event) => modifyValue(event));
+
+five.addEventListener('click', (event) => modifyValue(event));
+
+six.addEventListener('click', (event) => modifyValue(event));
+
+seven.addEventListener('click', (event) => modifyValue(event));
+
+eight.addEventListener('click', (event) => modifyValue(event));
+
+nine.addEventListener('click', (event) => modifyValue(event));
+
+zero.addEventListener('click', (event) => modifyValue(event));
+
+// Testing Modifier Buttons
+clear.addEventListener('click', (event) => resetResult(event));
+clearEntry.addEventListener('click', (event) => resetValue(event));
